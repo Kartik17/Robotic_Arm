3878d43e63b77f202a3329f5df21ea8b382d38c8
Kartik17 <32801164+Kartik17@users.noreply.github.com>
2018-09-08 07:31:45 +0530

    Updated
    Added Decorator to record function execution time.
    Added plot function to plot the links.
    Modified the code.

--- jacobian_inverse_kinematics.py
+++ jacobian_inverse_kinematics.py
@@ -2,8 +2,22 @@ import numpy as np
 from sympy import *
 from numpy import linalg as LA
 from numpy.linalg import inv
+import time
+import logging
 
+#################### Logging Function: Decorator ###################
 
+def note_time_func(use_func):
+    def wrapper_func(*args,**kwargs):
+        time_start = time.time()
+        result = use_func(*args,**kwargs)
+        delta_time = time.time() - time_start
+        print('Time to run the function is: {0:.2f}'.format(delta_time))
+        return result
+
+    return wrapper_func
+
+######################## Forward Kinematics ########################
 
 # Create Symbolic variables
 alpha0,alpha1,alpha2,alpha3,alpha4,alpha5,alpha6 = symbols('alpha0:7')
@@ -25,42 +39,66 @@ def TF_matrix(alpha,a,d,q):
                 [   0,  0,  0,  1]])
     return TF
 
-def jacobian_func(T,q):
-    jacobian_mat = []
-    for i in range(len(q)):
-        jacobian_mat.append(diff(T[:2,-1],q[i]).reshape(1,len(q)))
+######################## Inverse Kinematics ########################
 
+@note_time_func # Equivalent to: jacobian_func = note_time_func(jacobian_func)
+def jacobian_func(T,q):
+    jacobian_mat = [diff(T[:2,-1],q[i]).reshape(1,len(q)) for i in range(len(q))]
     jacobian_mat = Matrix(jacobian_mat).T
     return jacobian_mat
 
 T_01 = TF_matrix(alpha0,a0,d1,q1).subs(subs_dict)
 T_12 = TF_matrix(alpha1,a1,d2,q2).subs(subs_dict)
 T_23 = TF_matrix(alpha2,a2,d3,q3).subs(subs_dict)
+T_02 = T_01*T_12
 T_03 = T_01*T_12*T_23
 
-error = 1.0
-tolerance = 0.01
+# Important Note: If you skip the astype method, numpy will create a matrix of type 'object',
+# which won't work with common array operations.
 
-Q = np.matrix([[np.pi/3.],[np.pi/3.]])
+@note_time_func
+def iteration(T_03):
+    error = 1.0
+    tolerance = 0.01
 
-gen_gripper = T_03[:2,-1]
+    # Initial Guess - Joint Angles
+    Q = np.matrix([[0.],[np.pi/3.]])
+    # X,Y expression
+    gen_gripper = T_03[:2,-1]
+    # X,Y value for Target Position
+    target = np.matrix([[0.],[2.0]])
+    # Jacobian
+    jacobian = jacobian_func(T_03,[q1,q2])
+    jacobian_inv = jacobian.inv("LU")
 
-target = np.matrix([[0.],[2.0]])
+    while error> tolerance:
+        T_q = np.matrix(gen_gripper.evalf(subs = {q1: Q[0,0], q2: Q[1,0], q3: 0.})).astype(np.float64)
 
+        delta_T = target - T_q
 
-jacobian = jacobian_func(T_03,[q1,q2])
-jacobian_inv = jacobian.inv("LU")
+        Q = Q + np.matrix(jacobian_inv.evalf(subs = {q1: Q[0,0], q2: Q[1,0], q3: 0.})).astype(np.float64) * delta_T
 
-while error> tolerance:
-    #print('Check')
-    T_q = np.matrix(gen_gripper.evalf(subs = {q1: Q[0,0], q2: Q[1,0], q3: 0.})).astype(np.float64)
+        error = LA.norm(delta_T)
 
-    delta_T = target - T_q
+        print(error)
+    return Q
 
-    Q = Q + np.matrix(jacobian_inv.evalf(subs = {q1: Q[0,0], q2: Q[1,0], q3: 0.})).astype(np.float64) * delta_T
+Q = iteration(T_03)
+import matplotlib.pyplot as plt
 
-    error = LA.norm(delta_T)
+def plot_arm(Q,transform_matrices):
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111)
 
-    print(error)
+    # Check For Lambdify to Convert to Numpy matrix
+    x = [np.array(transform_matrices[i].evalf(subs = {q1: Q[0,0], q2: Q[1,0], q3: 0.})[0,-1]).astype(np.float64) for i in range(len(transform_matrices))]
+    y = [np.array(transform_matrices[i].evalf(subs = {q1: Q[0,0], q2: Q[1,0], q3: 0.})[1,-1]).astype(np.float64) for i in range(len(transform_matrices))]
+    ax1.plot(x,y)
+    ax1.set_xlim(-2.5,2.5)
+    ax1.set_ylim(-2.5,2.5)
+    ax1.grid(linestyle ='--')
+    plt.show()
+
+plot_arm(Q,[T_01,T_02,T_03])
 print(Q)
 print(np.matrix(T_03.evalf(subs = {q1: Q[0,0], q2: Q[1,0], q3: 0.})[:2,-1]))
